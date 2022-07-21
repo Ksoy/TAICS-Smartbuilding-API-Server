1fd9c5cf76abcb77e12a5b90915c87050a7f9d43
ksoy <ksoyam95@gmail.com>
2022-07-21 15:22:17 +0800

    feat: update decorator

--- subsystem/devices_app/app.py
+++ subsystem/devices_app/app.py
@@ -3,35 +3,33 @@ import random
 
 from datetime import datetime
 
-from flask import Blueprint, request
-from pytz import timezone
+from flask import Blueprint
 
 from ..db import models
-from ..utils import ntp_decorator
+from ..utils import response_decorator
 
 devices_app = Blueprint('devices', __name__)
 logger = logging.getLogger(__name__)
 
 
 @devices_app.route('/', methods=['GET'], endpoint='list')
-@ntp_decorator
+@response_decorator
 def devices_list():
     device_records = models.Device.query.all()
 
     return {
-        'self': request.url,
         'kind': 'Collection',
-        'timestamp': datetime.now(timezone('Asia/Taipei')).isoformat(),
         'devices': [d.export() for d in device_records],
     }
 
 
 @devices_app.route('/<string:device_ids>/value', methods=['GET'], endpoint='get')
-@ntp_decorator
+@response_decorator
 def devices_get_value(device_ids):
     return {'value': random.random()}
 
 
 @devices_app.route('/<string:device_ids>/value', methods=['POST'], endpoint='post')
+@response_decorator
 def devices_post_value(device_ids):
     return device_ids
--- subsystem/utils.py
+++ subsystem/utils.py
@@ -1,13 +1,16 @@
 import ntplib
 
-from flask import jsonify
+from datetime import datetime
+
+from flask import jsonify, request
+from pytz import timezone
 
 from . import config
 
 ntp_client = ntplib.NTPClient()
 
 
-def ntp_decorator(f):
+def response_decorator(f):
     def wrap(*arg, **args):
         if config.IS_TESTING:
             recv_time = get_ntp_tx()
@@ -15,11 +18,19 @@ def ntp_decorator(f):
         result = f(*arg, **args)
 
         if type(result) is dict:
+            result.update({
+                'self': request.url,
+                'timestamp': datetime.now(timezone('Asia/Taipei')).isoformat(),
+            })
+
             if config.IS_TESTING:
-                result['ntp'] = {
-                    'server_recv_time': recv_time,
-                    'server_send_time': get_ntp_tx(),
-                }
+                result.update({
+                    'ntp': {
+                        'ntp_server': config.NTP_SERVER,
+                        'api_recv_time': recv_time,
+                        'api_send_time': get_ntp_tx(),
+                    }
+                })
 
             return jsonify(result)
         return result
