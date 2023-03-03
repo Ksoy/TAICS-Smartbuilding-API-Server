40dad1a0adc5de4a96c523936e55232297d099a6
Ksoy <ksoyam95@gmail.com>
2023-03-03 11:41:32 +0800

    refactor: simplify initdb

--- subsystem/app.py
+++ subsystem/app.py
@@ -27,6 +27,7 @@ def start_server():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{config.BASE_DIR}/sqlite.db'
     # Ref: https://tinyurl.com/9umn83fe
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
     with app.app_context():
         db.create_all()
@@ -48,25 +49,12 @@ def init_db():
             tag=d.get('tag'),
             desc=d.get('desc'),
             type=d.get('type'),
-            locName=d.get('loc', {}).get('locName'),
-            locDesc=d.get('loc', {}).get('locDesc'),
-            lat=d.get('loc', {}).get('lat'),
-            lon=d.get('loc', {}).get('lon'),
-            hgt=d.get('loc', {}).get('hgt'),
-            elev=d.get('loc', {}).get('elev'),
-            roomTag=d.get('loc', {}).get('roomTag'),
-            floorName=d.get('loc', {}).get('floorName'),
-            spaceName=d.get('loc', {}).get('spaceName'),
-            vendor=d.get('meta', {}).get('vendor'),
-            model=d.get('meta', {}).get('model'),
-            SN=d.get('meta', {}).get('SN'),
-            fmwVer=d.get('meta', {}).get('fmwVer'),
-            appVer=d.get('meta', {}).get('appVer'),
-            URL=d.get('meta', {}).get('URL'),
-            spec=d.get('meta', {}).get('spec'),
+            **d.get('loc', {}),
+            **d.get('meta', {}),
         )
         db.session.add(new_device)
         db.session.commit()
+
         for short_name, p in d.get('Properties', {}).items():
             new_property = models.Property(
                 shortName=short_name,
@@ -77,11 +65,13 @@ def init_db():
             )
             db.session.add(new_property)
             db.session.commit()
+
             if 'value' in p:
                 new_value = models.Value(
                     PropertyID=new_property.id,
                     value=p.get('value')
                 )
                 db.session.add(new_value)
+                db.session.commit()
 
     db.session.commit()
